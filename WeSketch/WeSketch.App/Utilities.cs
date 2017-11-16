4dd0a03befb025c88550529b7b81d91dabdbc2aa
Stevica Stojković <hochopepa@mail.com>
2017-11-17 00:56:35 +0100

    Added class with utility functions

---
+++ WeSketch/WeSketch.App/Utilities.cs
@@ -0,0 +1,18 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeSketch.App
+{
+    public static class Utilities
+    {
+        public static async void DisplayMessage(MetroWindow window, string title, string message)
+        {
+            await window.ShowMessageAsync(title, message);
+        }
+    }
+}
