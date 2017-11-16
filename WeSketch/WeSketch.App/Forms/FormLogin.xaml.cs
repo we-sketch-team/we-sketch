a558085efdc50770af8dc62e1d9a17cdee4acde5
Stevica Stojković <hochopepa@mail.com>
2017-11-17 00:56:59 +0100

    Added Login form

---
+++ WeSketch/WeSketch.App/Forms/FormLogin.xaml.cs
@@ -0,0 +1,41 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WeSketch.App.Forms
+{
+    /// <summary>
+    /// Interaction logic for FormLogin.xaml
+    /// </summary>
+    public partial class FormLogin : MetroWindow
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            FormRegister reg = new FormRegister();
+            reg.Show();
+            
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Utilities.DisplayMessage(this, "Can't login", "Not implemented yet!");
+        }
+    }
+}
