f6cde85d135d577e45e87b8ca53ccb725523d052
Stefan Stanković <stefanstanko@elfak.rs>
2018-02-04 21:38:50 +0100

    WeSketch.Common: remote methods lists

---
+++ WeSketch/WeSketch.Common/RemoteMethods/UserHubMethods.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeSketch.Common.RemoteMethods
+{
+	public static class UserHubMethods
+	{
+		public static string Login
+		{
+			get
+			{
+				return "Login";
+			}
+		}
+		public static string CreateAccount
+		{
+			get
+			{
+				return "CreateAccount";
+			}
+		}
+		public static string GetUser
+		{
+			get
+			{
+				return "GetUser";
+			}
+		}
+		public static string UpdateUser
+		{
+			get
+			{
+				return "UpdateUser";
+			}
+		}
+		public static string GetUserByUsername
+		{
+			get
+			{
+				return "GetUserByUsername";
+			}
+		}
+		public static string DeleteUser
+		{
+			get
+			{
+				return "DeleteUser";
+			}
+		}
+	}
+}
