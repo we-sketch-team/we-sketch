9c8eb70920eb139be323f1ad09b26a715c6e3eff
Stefan Stanković <stefanstanko@elfak.rs>
2018-04-01 20:03:03 +0200

    Syncronizer fix

---
+++ WeSketch/WeSketch.Common/CommonClasses/CreateActionSyncerDataModifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeSketch.Common.CommonClasses
+{
+	public class CreateActionSyncerDataModifier : SyncerDataModifier
+	{
+		public CreateActionSyncerDataModifier(SyncerData previousData) : base(previousData)
+		{
+				
+		}
+
+		public override void Modify(CommonBoard board)
+		{
+			data.BoardsToCreate.Add(board);			
+		}
+	}
+}
