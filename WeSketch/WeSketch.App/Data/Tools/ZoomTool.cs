62b091b62d590462bacadded668978ec205b8041
Stevica Stojković <hochopepa@mail.com>
2018-04-04 21:43:40 +0200

    Added Zoom tools

---
+++ WeSketch/WeSketch.App/Data/Tools/ZoomTool.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using WeSketch.App.Controller;
+using WeSketch.App.Forms;
+
+namespace WeSketch.App.Data.Tools
+{
+    public class ZoomTool : ITool
+    {
+        protected IDrawable workspace;
+        protected double modifier = 0.1;
+        protected static double Scale = 1.0;
+
+        public ZoomTool(IDrawable workspace)
+        {
+            this.workspace = workspace;
+        }
+
+        public void Activate()
+        {
+            var canvas = workspace.GetCanvas();
+            Scale = CalculateScale(Scale);
+            ScaleTransform scaleT = new ScaleTransform(Scale, Scale);
+            canvas.RenderTransform = scaleT;
+        }
+
+        protected virtual double CalculateScale(double scale)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deactivate()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void MouseDown(int x, int y)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void MouseUp(int x, int y)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void SetController(IWorkspaceController controller)
+        {
+            //
+        }
+    }
+}
